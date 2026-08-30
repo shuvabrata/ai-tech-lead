0be6ffccea695d10344cb7bf5faa5125d6ad107d
Shuva Brata Deb <shuvabrata@gmail.com>
2026-08-30 13:45:52 +0530

    reduce margin

--- src/app/dash_app/styles.py
+++ src/app/dash_app/styles.py
@@ -354,7 +354,7 @@ COMPACT_PAGE_HEADER_STYLE = {
     "letterSpacing": "0.8px",
     "borderBottom": f"1px solid {COLOR_BORDER}",
     "paddingBottom": SPACING_XSMALL,
-    "marginBottom": SPACING_XSMALL
+    "marginBottom": SPACING_XXSMALL
 }
 
 # Card Containers
